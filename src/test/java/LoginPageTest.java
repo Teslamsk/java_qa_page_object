e4be52cdb70940959f20b1edba6125d95c988322
Alexander Bodoin <aleksandr1borodin@gmail.com>
2020-03-17 01:24:01 +0300

    first commit

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginPageTest {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
+        System.setProperty("webdriver.firefox.bin", "D:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://github.com/login");
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    public void loginWithEmptyCredsTest(){
+        LoginPage newLoginPage =  loginPage.loginWithInvalidCreds("","");
+        String error = newLoginPage.getErrorText();
+        Assert.assertEquals("Incorrect username or password.", error);
+    }
+
+
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
 }
